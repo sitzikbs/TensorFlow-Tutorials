693a6a5d14f614282931449dbe189d6341c4b39e
Itzik Ben Shabat <sitzikbs@gmail.com>
2017-02-16 18:14:22 +0200

    updated Beginners file to tensorflow 1.0

--- MNIST4Beginners.py
+++ MNIST4Beginners.py
@@ -19,7 +19,7 @@ b = tf.Variable(tf.zeros([10]))
 y = tf.nn.softmax( tf.matmul(x,W) + b )
 
 y_ = tf.placeholder( tf.float32, [None, 10] )
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, y_))
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y, labels=y_))
 
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
